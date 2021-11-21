86efc7e2acca2075433e14c45654cd88ce2dd26a
Charlie <charliecarrier@gmail.com>
2021-11-21 15:29:58 -0500

    V1 of placing a unit

--- src/blitzkrieg_solo/utils.clj
+++ src/blitzkrieg_solo/utils.clj
@@ -2,8 +2,29 @@
 
 (defn calculate-effect [effect])
 
-(defn calculate-score [game-state])
+(defn place-unit-in-campaign [game-state theater-key campaign-pos battle-space-pos unit]
+  (let [theaters (:theaters game-state)
+        theater (theater-key theaters)
+        campaigns (:campaigns theater)
+        campaign-pos-key (keyword (str campaign-pos))
+        campaign (campaign-pos-key campaigns)
+        battle-spaces (:battle-spaces campaign)
+        battle-space-pos-key (keyword (str battle-space-pos))
+        battle-space (battle-space-pos-key battle-spaces)
+        updated-battle-space (assoc battle-space :unit unit)
+        updated-battle-spaces (assoc battle-spaces battle-space-pos-key updated-battle-space)
+        updated-campaign (assoc campaign :battle-spaces updated-battle-spaces)
+        updated-campaigns (assoc campaigns campaign-pos-key updated-campaign)
+        updated-theater (assoc theater :campaigns updated-campaigns)
+        updated-theaters (assoc theaters theater-key updated-theater)
+        updated-game-state (assoc game-state :theaters updated-theaters)]
+
+    updated-game-state
+    )
+  )
+
+(defn calculate-score [game-state]
+  )
 
 (defn make-player-move [game-state combatant unit theater campaign position]
-
   )
